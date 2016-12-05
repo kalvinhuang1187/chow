f8c3f898584deb6a4cbeba6fe4dac620aea8aa1c
Kalvin Huang <kalvin_huang@yahoo.com>
2016-12-04 19:19:59 -0800

    Finish three more functions

--- src/chowquiz.py
+++ src/chowquiz.py
@@ -1,4 +1,5 @@
 import pdb
+import re
 '''
 Using Python 2.7 or 3.3+ syntax/semantics, please fill out the bodies of
 the included functions to include implementations of what is described in the
@@ -10,10 +11,55 @@ def list_of_integers(start=23, end=100):
     Returns a list of integers from 23 to 100 that are evenly divisible by 7.
     '''
     result = []
-    for i in range (start, end):
+    for i in range (start, end+1):
         if i % 7 == 0:
             result.append(i)
     return result
+
+
+def dict_mapping():
+    '''
+    Returns a dictionary mapping integers to their 2.5th root for all integers
+    from 2 up to 100 (including 100).
+    '''
+    dictionary = {}
+    for i in range(2, 100+1):
+        compute = i**(1/2.5)
+        dictionary[i] = compute
+
+    return dictionary
+
+
+def find_ips(inp):
+    '''
+    Returns a list of valid ip (v4) addresses of the form 'x.x.x.x'
+    that are in the input string (separated by at least some whitespace).
+    >>> find_ips('this has one ip address 127.0.0.1')
+    ['127.0.0.1']
+    '''
+
+    pat = re.compile("\d{1,3}.\d{1,3}.\d{1,3}.\d{1,3}")
+    test = pat.findall(inp)
+
+    return test
+
+
+def generate_cubes_until(modulus):
+    '''
+    Generates the cubes of integers greater than 0 until the next is 0 modulo
+    the provided modulus.
+    >>> list(generate_cubes_until(25))
+    [1, 8, 27, 64]
+    '''
+    result = []
+    count = 1
+
+    while ( ((count**3) % modulus != 0) & (count < 100) ):
+        result.append(count**3)
+        count += 1
+
+    return result
+
 
 def dummy():
     #pdb.set_trace()
--- test/src/test_chowquiz.py
+++ test/src/test_chowquiz.py
@@ -1,12 +1,31 @@
 import unittest
-from src.chowquiz import list_of_integers, dummy
+from src.chowquiz import list_of_integers, dict_mapping, find_ips, generate_cubes_until, dummy
 
 def test_list_of_integers():
-    sample = list_of_integers(1,10)
-    assert sample == [7]
+    sample = list_of_integers(1,14)
+    assert sample == [7,14]
 
     default_list = list_of_integers()
     assert default_list == [28, 35, 42, 49, 56, 63, 70, 77, 84, 91, 98] 
+
+def test_dict_mapping():
+	output = dict_mapping()
+	assert len(output) == 99
+	assert output[2] == 1.3195079107728942
+	assert output[100] == 6.309573444801933
+
+def test_find_ips():
+	string = "this has one ip address 127.0.0.1"
+	ip = find_ips(string)
+	assert ip == ['127.0.0.1']
+
+	string2 = "this has one ip address 127.0.0.1, but maybe two 0.0.0.0"
+	ip2 = find_ips(string2)
+	assert ip2 == ['127.0.0.1', '0.0.0.0']
+
+def test_generate_cubes_until():
+	cubes = generate_cubes_until(25)
+	assert cubes == [1, 8, 27, 64]
 
 def test_dummy():
     subject = dummy() 
